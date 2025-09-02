243c2d3d24c80dd60a210baa8b5f1ef3606772fa
root <worleymr@gmail.com>
2025-09-02 00:04:14 +0000

    Added configuration for the VM

--- src/openweb_enricher/webapp.py
+++ src/openweb_enricher/webapp.py
@@ -350,4 +350,4 @@ def download(job_id, fmt):
         return "Unsupported format", 400
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
